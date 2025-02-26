8b539ca636ff98d45211bb57cd71ad78f0f9ab4e
Sebastian Q <sebastian.quinones.inf@gmail.com>
2025-02-26 08:39:34 -0300

    Initial Commit - Clean Startup Project

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import "../styles/main.scss";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="row align-items-center">
+        <div className="col">
+          <h1>Clean Startup Project</h1>
+          <p>A modern React + TypeScript starter template.</p>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Home;
